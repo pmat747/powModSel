81a659f49ea07c50bdd7aa946e2f31107ca46eda
Patricio Maturana Russel <33706128+pmat747@users.noreply.github.com>
2018-11-28 18:20:15 +1300

    update

--- R/liketrace.R
+++ R/liketrace.R
@@ -7,32 +7,25 @@
 
 liketrace = function(x, temp = NULL){
 
-  # x  : data (1 column: temperatures, 2 column likelihood values)
-  # temp: temperature to be plotted
-
   index = which(diff(x$invTemp)!=0);
   index = cbind(c(1, index + 1), c(index, dim(x)[1]));
-  j     = dim(index)[1];
+
+  if(is.null(temp)){
+
+    j    = dim(index)[1];
+    temp = 1:j;
+  }
 
   tempvalue  = round(unique(x$invTemp), 4);
 
-  if(!is.null(temp)){
+  for(i in temp){
 
-    ts.plot(x$logL[index[temp, 1]:index[temp, 2]],
-            main = paste( "Inverse Temperature ", tempvalue[temp], " - ", temp),
+    aux = index[i,1]:index[i,2];
+    ts.plot(x$logL[aux],
+            main = paste( "Inverse Temperature ", tempvalue[i], " - ", i),
             ylab = "log-likelihood", xlab = "Iteration");
-
-  }else{
-
-    for(i in 1:j){
-
-      aux = index[i,1]:index[i,2];
-      ts.plot(x$logL[aux],
-              main = paste( "Inverse Temperature ", tempvalue[i], " - ", i),
-              ylab = "log-likelihood", xlab = "Iteration");
-    }
-
-    cat("Number of traceplots", j);
-
   }
+
+  cat("Number of traceplots", length(temp));
+
 }
